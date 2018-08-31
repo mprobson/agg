a9d7b9df163ebe9cfe846cb09a2ca38af9491e59
Michael Robson <mprobson@illinois.edu>
2018-08-31 15:40:23 -0500

    outer: Sketch code
    Change-Id: I2f3940f133d791d44dbe9befacb983f1a9485d0b

--- shared/outer/outer.cu
+++ shared/outer/outer.cu
@@ -1,12 +1,35 @@
 // Description
+//  Outer product
 // TODO
-// - define to typedef?
+// - Kernel Sizes
 // - Timings
+// Future
 // - Correctness
+// - (CUDA) Error Checking
+// - Checking and Generating Doubles
+#include <cstdlib>
+#include <ctime>
+#include <curand_kernel.h>
 
-#define precision_t float
+typedef float precision_t;
 
-__global__ 
+__global__
+void init(unsigned int seed, curandState* state) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  curand_init(seed, idx, 0, &state[id]);
+}
+
+__global__
+void generate(precision_t* m, size_t m, int rMax, curandState* state) {
+  int idx = blockId.x * blockDim.x + threadIdx.x;
+  curandState localState = state[id];
+  for (int i = idx; i < m ; i += gridDim.x) {
+    m[i] = curand_uniform(&localState) * rMax;
+  }
+  state[id] = localState;
+}
+
+__global__
 void outer(precision_t* m, precision_t* n, precision_t* mn) {
 }
 
@@ -14,43 +37,63 @@ int main(int argc, char* argv[]) {
   // Defaults
   // - k kernels
   int k = 1;
+
   // - m data on GPU
   size_t m = 1000;
-  precision_t* d_m;
+
   // - n data on host
   size_t n = 1000;
-  precision_t* h_n;
-  precision_t* d_n;
-  // - mn final matrix
-  precision_t d_mn;
-  precision_t h_mn;
+
+  // - r max number
+  int rMax = 100;
+
+  unsigned int hSeed = std::time(NULL);
+  unsigned int dSeed = hSeed;
 
   // Process Input
   if (argc > 1) {
     // TODO
   }
+
   // Allocate Memory
-  // - Device
-  cudaMalloc(&d_m, m * sizeof(precision_t));
-  // - Host
 #if 0
   cudaMallocManaged();
   malloc();
 #else
+  // - Device
+  precision_t* d_m;
+  cudaMalloc(&d_m, m * sizeof(precision_t));
+
+  // - Host
+  precision_t* h_n;
   cudaMallocHost(&h_n, n * sizeof(precision_t));
+  precision_t* d_n;
   cudaMalloc(&d_n, n * sizeof(precision_t));
-#endif // 1
+
   // - Matrix
+  precision_t h_mn;
   cudaMallocHost(&h_mn, m * n * sizeof(precision_t));
+  precision_t d_mn;
   cudaMalloc(&d_mn, m * n * sizeof(precision_t));
+
+  curandState* d_states;
+  cudaMalloc(&d_states, * sizeof(curandState));
+#endif // 1
+
+  //init<<<>>>(dSeed, d_states);
 
   // Generate Two Vectors
   // - Device
-  // <<<kernel launch>>>
+  //generate<<<>>>(d_m, m, rMax, d_states);
+
   // - Host
+  std::srand(hSeed);
   for (int i = 0; i < n; i++) {
-    //h_n[i] = rand;
+    h_n[i] = std::rand() / ((RAND_MAX + 1u) / rMax);
   }
+
+  // Ensure random number generation finishes
+  cudaDeviceSynchronize();
 
   // Copy Vector
 #if 0
@@ -70,5 +113,11 @@ int main(int argc, char* argv[]) {
 #endif // 1
 
   // Do something with the data to prevent optimization
-  // mutiply by scalalr and print? Do some norm/reduction?
+  // mutiply by scalar and print? Do some norm/reduction?
+
+  cudaFree(d_m);
+  cudaFree(d_n);
+  cudaFree(d_mn);
+  cudaFree(d_states);
+  // free/cudaFree h_*
 }
